83c8a301fc3cef9e04837d2b7c4b046dc60f264e
Wai Kennedy <kennedywai@hotmail.com>
2020-11-13 19:03:03 +0800

    Update ros-melodic-installation-hwr.sh

--- ros-melodic-installation-hwr.sh
+++ ros-melodic-installation-hwr.sh
@@ -20,11 +20,3 @@ sudo apt install python-rosdep
 sudo rosdep init
 rosdep update
 sudo reboot
-# Create a ROS Workspace
-#mkdir -p ~/catkin_ws/src
-#cd ~/catkin_ws/
-#catkin_make
-# source new setup file
-#source ~/catkin_ws/devel/setup.bash
-#echo $ROS_PACKAGE_PATH
-#echo "source $HOME/catkin_ws/devel/setup.bash" >> ~/.bashrc
